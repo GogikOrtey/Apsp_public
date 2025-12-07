71baac6275abdd99ed3e7521013d79c9e1c7977b
Gogik <gog.ortey@yandex.ru>
2025-12-07 06:40:00 +0500

    Прописал планы на будущее

--- test.js
+++ test.js
@@ -4,31 +4,6 @@ async parseCard(set: SetType, cacher: Cacher<ResultItem[]>) {
     const data = await this.makeRequest(set.query);
     const $ = cheerio.load(data);
 
-    const stock = $(".nal.y").text()?.includes("есть на складе") ? "InStock" : "OutOfStock"
-    const name = $("h1.name").text()?.trim()
-    const price = $(".b").text()?.trim()
-    const article = $(".char > p:nth-of-type(1)").text()?.trim()
-    const brand = $(".char > p:nth-of-type(2)").text()?.trim()
-    const imageLink = $("html > body > section.wrap > main > article.wide > .card > .img_bl > .img > a.fancybox[href]")?.attr("href")?.trim()
-    const oldPrice = $(".thr").text()?.trim()
-    const link = set.query
-    const timestamp = getTimestamp()
-
-    const item: ResultItem = {
-        name, price, article, brand, imageLink, oldPrice, stock, timestamp
-    }
-    items.push(item);
-
-    cacher.cache = items
-    return items;
-}
-
-async parseCard(set: SetType, cacher: Cacher<ResultItem[]>) {
-    let items: ResultItem[] = []
-
-    const data = await this.makeRequest(set.query);
-    const $ = cheerio.load(data);
-
     const stock = $(".nal.y").text()?.includes("есть на складе") ? "InStock" : "OutOfStock"   
     const name = $("h1.name").text()?.trim() 
     const price = $(".b").text()?.trim()     
@@ -40,10 +15,45 @@ async parseCard(set: SetType, cacher: Cacher<ResultItem[]>) {
     const timestamp = getTimestamp()
 
     const item: ResultItem = {
-        name, price, article, brand, imageLink, oldPrice, stock, timestamp
+        name, price, article, brand, imageLink, oldPrice, stock, timestamp, link
     }
     items.push(item);
 
     cacher.cache = items
     return items;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
