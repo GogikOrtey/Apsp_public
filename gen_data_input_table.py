68b6645f78c7e80c25b080b9269e7f76dd13d70f
Gogik <gog.ortey@yandex.ru>
2025-12-15 04:49:09 +0500

    +

--- gen_data_input_table.py
+++ gen_data_input_table.py
@@ -1,10 +1,9 @@
 # Автогенерация из шага 4. Не редактируйте вручную.
-# Сохранено: 2025-12-15 04:42:11
+# Сохранено: 2025-12-15 04:46:21
 import json
 
 data_input_table = json.loads(r'''{
   "host": "https://c-s-k.ru",
-  "timestamp": 1765755725,
   "fields_str": "",
   "links": {
     "simple": [
@@ -18,19 +17,6 @@ data_input_table = json.loads(r'''{
         "OutOfStock_trigger": ""
       }
     ]
-  },
-  "search_requests": [
-    {
-      "query": "дверь",
-      "url_search_query_page_2": "https://c-s-k.ru/catalog/?q=%D0%B4%D0%B2%D0%B5%D1%80%D1%8C&s=%D0%9F%D0%BE%D0%B8%D1%81%D0%BA&PAGEN_1=2",
-      "count_of_page_on_pagination": "20",
-      "total_count_of_results": "0",
-      "links_items": [
-        "https://c-s-k.ru/catalog/dveri_okna_i_skobyanye_izdeliya/dveri_mezhkomnatnye/dveri_laminirovannye/131955/",
-        "https://c-s-k.ru/catalog/dveri_okna_i_skobyanye_izdeliya/dveri_vkhodnye/253596/",
-        "https://c-s-k.ru/catalog/dveri_okna_i_skobyanye_izdeliya/dveri_vkhodnye/246297/"
-      ]
-    }
-  ]
+  }
 }
 ''')
