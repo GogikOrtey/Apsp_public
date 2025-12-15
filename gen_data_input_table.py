fa468e8da1d44da57c06ca4d86d9425c0f008b9a
Georgiy Orlov <orlov-ga@brandpolgroup.com>
2025-12-15 14:03:23 +0500

    +

--- gen_data_input_table.py
+++ gen_data_input_table.py
@@ -1,45 +1,47 @@
 # Автогенерация из шага 4. Не редактируйте вручную.
-# Сохранено: 2025-12-15 13:29:27
+# Сохранено: 2025-12-15 13:58:22
 import json
 
 data_input_table = json.loads(r'''{
-  "host": "https://kotel-nasos.ru",
-  "timestamp": 1765787339,
+  "host": "https://stroytorg812.ru",
+  "timestamp": 1765789096,
   "fields_str": "",
   "links": {
     "simple": [
       {
-        "link": "https://kotel-nasos.ru/napolnyy-gazovyy-kotel-60-kvt-baxi-slim-1-620in-9e/",
-        "name": "Напольный газовый котел 60 кВт Baxi SLIM 1.620 iN 9E",
-        "price": "195 000 ₽",
-        "oldprice": "238 150 ₽",
-        "imageLink": "https://kotel-nasos.ru/wa-data/public/shop/products/17/01/30117/images/53793/53793.440.jpg",
-        "brand": "Baxi",
-        "InStock_trigger": "В наличии",
+        "link": "https://stroytorg812.ru/catalog/smesiteli/smesitel_dlya_vanny_potato_p3016_korotkiy_izliv/",
+        "name": "Смеситель для ванны Potato P3016 короткий излив ## POTATO",
+        "price": "Цена: 3 250,00 руб.",
+        "oldprice": "старая цена: 4 290,00 руб.",
+        "imageLink": "https://stroytorg812.ru/upload/resize_cache/iblock/b40/325_325_1/db237327_a6bb_11ec_a995_002590746688_459674ac_b3ee_11ec_a6a2_002590746688.jpeg",
+        "article": "P3016",
+        "brand": "POTATO",
+        "InStock_trigger": "есть на складе",
         "OutOfStock_trigger": ""
       },
       {
-        "link": "https://kotel-nasos.ru/gazovyy-nakopitelnyy-vodonagrevatel-ariston-sga-200-r/",
-        "name": "Газовый накопительный водонагреватель Ariston SGA 200 R",
-        "price": "85 000 ₽",
-        "oldprice": "95 500 ₽",
-        "imageLink": "https://kotel-nasos.ru/wa-data/public/shop/products/06/57/5706/images/10101/10101.440.jpg",
-        "brand": "Ariston",
-        "InStock_trigger": "",
-        "OutOfStock_trigger": "Предзаказ"
+        "link": "https://stroytorg812.ru/catalog/vanny/vanna_akrilovaya_150kh70_evropa_/",
+        "name": "Ванна акриловая 150х70 Европа # ТРИТОН",
+        "price": "Цена: 6 990,00 руб.",
+        "oldprice": "старая цена: 10 990,00 руб.",
+        "imageLink": "https://stroytorg812.ru/upload/resize_cache/iblock/d69/325_325_1/24bea462_7753_11f0_8c17_002590746688_24bea46d_7753_11f0_8c17_002590746688.jpeg",
+        "article": "ST26",
+        "brand": "ТРИТОН",
+        "InStock_trigger": "есть на складе",
+        "OutOfStock_trigger": ""
       }
     ]
   },
   "search_requests": [
     {
-      "query": "Котёл",
-      "url_search_query_page_2": "https://kotel-nasos.ru/search/?page=2&query=%D0%9A%D0%BE%D1%82%D1%91%D0%BB",
-      "count_of_page_on_pagination": "115",
-      "total_count_of_results": "0",
+      "query": "Ванна",
+      "url_search_query_page_2": "https://stroytorg812.ru/content/search/?s=&q=%D0%92%D0%B0%D0%BD%D0%BD%D0%B0&PAGEN_1=2",
+      "count_of_page_on_pagination": "0",
+      "total_count_of_results": "3 802",
       "links_items": [
-        "https://kotel-nasos.ru/elektricheskiy-kotel-evan-epo-pro-54/",
-        "https://kotel-nasos.ru/elektricheskiy-kotel-evan-epo-pro-48/",
-        "https://kotel-nasos.ru/elektricheskiy-kotel-evan-epo-pro-42/"
+        "https://stroytorg812.ru/catalog/mebel_dlya_vannoy/tumba_napolnaya_vega_60_s_umyvalnikom_vega_60_belaya/",
+        "https://stroytorg812.ru/catalog/mebel_dlya_vannoy/esper_60_tumba_podvesnaya1_yashchik_tsvet_belyy_s_umyvalnikom_esp60w0i95k/",
+        "https://stroytorg812.ru/catalog/mebel_dlya_vannoy/tumba_napolnaya_vega_70_s_umyvalnikom_vega_70_belaya/"
       ]
     }
   ]
