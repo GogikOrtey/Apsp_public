1749d70895fe02b7e49f97468343b74be2b90eaf
Georgiy Orlov <orlov-ga@brandpolgroup.com>
2025-12-19 16:37:18 +0500

    +

--- use_proxy.py
+++ use_proxy.py
@@ -1,13 +1,15 @@
 import requests
 
-proxy = "http://10.0.4.112:1020"  # если прокси без авторизации
-# proxy = "http://user:password@10.0.4.112:1050"  # если требуется логин/пароль
+DEFAULT_PROXY = "http://10.0.4.112:1020"
 
-proxies = {
-    "http": proxy,
-    "https": proxy,
-}
+def request_for_proxy(url: str, proxy: str = DEFAULT_PROXY, timeout: int = 10) -> str:
+    proxies = {
+        "http": proxy,
+        "https": proxy,
+    }
+    resp = requests.get(url, proxies=proxies, timeout=timeout)
+    resp.raise_for_status()
+    return resp.text
 
-resp = requests.get("https://api.ipify.org", proxies=proxies, timeout=10)
-resp.raise_for_status()
-print("IP через прокси:", resp.text)
+
+print("IP через прокси:", request_for_proxy("https://api.ipify.org"))
